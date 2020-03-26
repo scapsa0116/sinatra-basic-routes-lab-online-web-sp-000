4bd7fc4ed18ca7506b45dc577c9075650ec46ae7
Stela Capsa <stela.capsa@gmail.com>
2020-03-26 13:04:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do 
+    "My name is Stela" 
+  end 
+  
+  get '/hometown' do 
+   "My hometown is Chicago" 
+  end 
+  
+  get '/favorite-song' do 
+   "My favorite song is Daniel Pertsev - You" 
+  end 
 end
