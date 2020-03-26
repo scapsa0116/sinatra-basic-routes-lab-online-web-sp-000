477d6a8f116296757e8cb48fbe22370be0021785
Stela Capsa <stela.capsa@gmail.com>
2020-03-26 12:55:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do 
-    "Hello, World!"
+  get '/name' do 
+    "Stela Capsa"
+  end 
+  
+  get '/hometown' do 
+    "Chicago"
+  end 
+  
+  get '/favorite-song' do 
+    "Daniel Pertsev - You"
   end 
 end
